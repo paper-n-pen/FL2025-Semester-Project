48fd6dca423a62a26dbb752f3916e297fc699847
paper-n-pen <brokenrectangle@gmail.com>
2025-09-22 22:51:37 -0500

    Database configuration file added

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+// backend/db.js
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const pool = new Pool({
+  user: process.env.DB_USER || 'myapp_user',
+  host: 'localhost',
+  database: process.env.DB_NAME || 'myapp_db',
+  password: process.env.DB_PASSWORD || 'secret',
+  port: 5432,
+});
