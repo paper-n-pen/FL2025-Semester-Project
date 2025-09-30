f2e7eb191ddefce2f7ad96c6bd690de82551d70b
paper-n-pen <brokenrectangle@gmail.com>
2025-09-29 20:42:13 -0500

    Home page adjustments

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,18 +1,24 @@
 // my-react-app/src/App.tsx
-
 import { Routes, Route, Link } from 'react-router-dom';
 import Register from './Register';
 import Login from './Login';
+import Dashboard from './Dashboard'; // You’ll create this next
 
 function App() {
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/Register">Register</Link> | <Link to="/login">Login</Link>
+        <Link to="/">Home</Link> | <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
       </nav>
+
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
