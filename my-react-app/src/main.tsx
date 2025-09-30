b6167c73fe44df3797a8f60b43e054a02c68a698
paper-n-pen <brokenrectangle@gmail.com>
2025-09-29 21:17:53 -0500

    Changed comment

--- my-react-app/src/main.tsx
+++ my-react-app/src/main.tsx
@@ -1,16 +1,14 @@
-// my-react-app/src/main.tsx
+// my-react-app/src/Main.tsx
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import MainRouter from './MainRouter';
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-      <MainRouter />
-  </StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
